e1f1faa6e2ec69460732c5d4d5155f6394fd17f9
Tasdid <rahman.tasdid@gmail.com>
2019-09-25 16:31:51 +0600

    Added fragments and tabs

--- app/src/main/java/com/tasdid/instagramclone/SocialMeadiaActivity.java
+++ app/src/main/java/com/tasdid/instagramclone/SocialMeadiaActivity.java
@@ -1,14 +1,36 @@
 package com.tasdid.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+
+
 public class SocialMeadiaActivity extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_meadia);
+
+        setTitle("Social Media app!!!");
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
+
     }
 }
