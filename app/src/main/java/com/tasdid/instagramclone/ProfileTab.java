6be01c24c1df8b669528439b8b751f1a65c352df
Tasdid <rahman.tasdid@gmail.com>
2019-09-26 00:15:45 +0600

    Implemented Users Tab

--- app/src/main/java/com/tasdid/instagramclone/ProfileTab.java
+++ app/src/main/java/com/tasdid/instagramclone/ProfileTab.java
@@ -2,12 +2,19 @@ package com.tasdid.instagramclone;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -15,6 +22,10 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText edtProfileName, edtProfileBio, edtProfileProfession,
+                edtProfileHobbies, edtProfileFavoriteSport;
+
+    private Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -24,8 +35,100 @@ public class ProfileTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab,
+                container, false);
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtprofileHobbies);
+        edtProfileFavoriteSport = view.findViewById(R.id.edtProfileFavoriteSport);
+
+        btnUpdateInfo= view.findViewById(R.id.btnUpdateInfo);
+
+        //getting currentUser
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if(parseUser.get("profileName") == null){
+            edtProfileName.setText("");
+        }else{
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+
+        //getting currentUser info and setting in editText to show user
+
+        //getting user profile name
+        if(parseUser.get("profileName") == null){
+            edtProfileName.setText("");
+        }else{
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+        //getting user profile Bio
+        if(parseUser.get("profileBio") == null){
+            edtProfileBio.setText("");
+        }else{
+            edtProfileBio.setText(parseUser.get("profileBio").toString());
+        }
+
+        //getting user profile Profession
+        if(parseUser.get("profileProfession") == null){
+            edtProfileProfession.setText("");
+        }else{
+            edtProfileProfession.setText(parseUser.get("profileProfession").toString());
+        }
+
+        //getting user profile Hobbies
+        if(parseUser.get("profileHobbies") == null){
+            edtProfileHobbies.setText("");
+        }else{
+            edtProfileHobbies.setText(parseUser.get("profileHobbies").toString());
+        }
+
+        //getting user profile Hobbies
+        if(parseUser.get("profileFavoriteSport") == null){
+            edtProfileFavoriteSport.setText("");
+        }else{
+            edtProfileFavoriteSport.setText(parseUser.get("profileFavoriteSport").toString());
+        }
+
+
+
+
+        //Updating currentUser info from user input in editText
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName", edtProfileName.getText().toString());
+                parseUser.put("profileBio", edtProfileBio.getText().toString());
+                parseUser.put("profileProfession", edtProfileProfession.getText().toString());
+                parseUser.put("profileHobbies", edtProfileHobbies.getText().toString());
+                parseUser.put("profileFavoriteSport", edtProfileFavoriteSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+
+                            FancyToast.makeText(getContext(),  " Info Updated",
+                                    Toast.LENGTH_SHORT, FancyToast.INFO, false).show();
+                        }else{
+
+                            FancyToast.makeText(getContext(),  e.getMessage(),
+                                    Toast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
+
+
     }
 
 }
